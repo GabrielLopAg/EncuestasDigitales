fa32719fa3aac058ef840a607441840f7be2c0fe
Gabriel <gla280900@gmail.com>
2024-02-18 21:17:33 -0600

    add FloatLayout

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-
-
-class MyGrid(Widget):
-    name = ObjectProperty(None)
-    email = ObjectProperty(None)
-    
-    def btn(self):
-        print(self.name.text, self.email.text)
-        self.name.text = ""
-        self.email.text = ""
+from kivy.uix.floatlayout import FloatLayout
 
 
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return FloatLayout()
     
 
 if __name__ == "__main__":
     MyApp().run()    
 
 # The kv file needs to be call the same way that the main class (i.g. MyApp -> My.kv)
+# pos_hint: {"x", "y", "top", "botton", "left", "right"}
