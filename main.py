18405ed77f9c169550fe3d9603f31de658347af4
Gabriel <gla280900@gmail.com>
2024-04-23 23:45:16 -0600

    add window manage

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class MainWindow(Screen): # Login
+    pass
 
 
-class MyApp(App):
+class SecondWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager): # Transistion btw the windows
+    pass
+
+
+kv = Builder.load_file("my.kv")
+
+
+class MyMainApp(App):
     def build(self):
-        return FloatLayout()
+        return kv
     
 
 if __name__ == "__main__":
-    MyApp().run()    
+    MyMainApp().run()    
 
 # The kv file needs to be call the same way that the main class (i.g. MyApp -> My.kv)
 # pos_hint: {"x", "y", "top", "botton", "left", "right"}
